2b18c804255ad4fb4ce58d51a634c7d1bb42f69b
Arlen <arlenvargas2000@gmail.com>
2019-09-03 23:24:58 -0600

    Crear la entidad Phrase

---
+++ Entities/Phrase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+public class Phrase
+{
+    public int Id { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    int authorId { get; set; }
+
+    public Category category { get; set; }
+
+    public Author author { get; set; } 
+    
+}
